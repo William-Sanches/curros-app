032caa21d5d507dbaedeff622295205e2e82e3d1
William-Sanches <eng.williamsg@outlook.com>
2022-08-30 12:11:06 -0300

    final

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -19,6 +19,8 @@ export class HomeComponent implements OnInit {
 
   pontosCadastrados: Pontos[] = [];
 
+  cadastros: boolean = true;
+
   private service: IbgeService;
 
   constructor(public formBuilder: FormBuilder, service: IbgeService) {
@@ -42,6 +44,7 @@ export class HomeComponent implements OnInit {
       console.log(this.formCadastro);
       this.pontosCadastrados.push(this.formCadastro.value)
       console.log(this.pontosCadastrados);
+      this.cadastros = false;
     }
   }
 
